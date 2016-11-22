b542e8acc06ecabbed2f7475b521dce79981441b
polubis <polubis@wp.pl>
2016-11-22 19:38:16 +0100

    22 commit

--- Projekt restauracji/WpfApplication1/Faktura.xaml.cs
+++ Projekt restauracji/WpfApplication1/Faktura.xaml.cs
@@ -22,10 +22,10 @@ namespace WpfApplication1
     {
         public Faktura()
         {
-            InitializeComponent();
-            if(File.Exists(@"C:\Users\Adrianek\Desktop\Projekt restauracji\WpfApplication1\bin\Debug\\Faktura.txt"))
+            InitializeComponent();               // Kopiuje zawartosc pliku txt to Comboboxa
+            if(File.Exists("Faktura.txt"))
             {
-                StreamReader Odczyt = File.OpenText(@"C:\Users\Adrianek\Desktop\Projekt restauracji\WpfApplication1\bin\Debug\\Faktura.txt");
+                StreamReader Odczyt = File.OpenText("Faktura.txt");
                 string ZawartoscFaktury = Odczyt.ReadToEnd();
                 WyswietlFakture.Text = ZawartoscFaktury;
             }
